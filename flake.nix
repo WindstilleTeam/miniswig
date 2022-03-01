eb4fb61d2c776396e51d7183c0e2bebf8069109f
Ingo Ruhnke <grumbel@gmail.com>
2022-03-01 13:29:50 +0100

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Binding generator for Squirrel";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          miniswig = pkgs.stdenv.mkDerivation {
+            pname = "miniswig";
+            version = "0.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkgconfig
+            ];
+            buildInputs = [
+              pkgs.flex
+              pkgs.bison
+            ];
+           };
+        };
+        defaultPackage = packages.miniswig;
+      });
+}
