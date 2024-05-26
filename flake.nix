4565130ca81daa84372729e2df69636afe59cc94
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 15:36:19 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Binding generator for Squirrel";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
